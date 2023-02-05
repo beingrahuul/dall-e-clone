0e41cfb01a4372a423b7dcd0792d740c071236d3
Rahul kumar <bstshownow@gmail.com>
2023-02-05 20:55:22 +0530

    first-commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import BEINGRAHUUL from './BEINGRAHUUL.svg';
+import preview from './preview.png';
+
+export {
+  download,
+  logo,
+  preview,
+  BEINGRAHUUL
+};
